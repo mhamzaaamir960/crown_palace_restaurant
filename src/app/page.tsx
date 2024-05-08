81fc8bc33924d0f79dcd14185c06d524625c2968
muhammadhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-05-08 09:27:38 +0500

    updated

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      home page
+    </div>
+  );
+}
