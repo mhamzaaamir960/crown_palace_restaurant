b0410ee92b071bb421b90c80003a2a819d955213
muhammadhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-05-09 22:49:04 +0500

    updated

--- src/components/About/AboutDescription.tsx
+++ src/components/About/AboutDescription.tsx
@@ -6,7 +6,6 @@ async function AboutDescription() {
   const about = await data.items.filter(
     (item: any) => item.sys.contentType.sys.id === "aboutUs"
   );
-  console.log(about);
   return (
     <p className="text-gray-700 text-lg ">
       {about && about[0].fields.about}
