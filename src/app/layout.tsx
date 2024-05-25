c4f3735af1184c786bb84c4afd8dd46ef4aa6975
muhammadhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-05-25 09:25:40 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Navbar, Footer } from "@/components/index";
 import { getHeroSectionData } from "@/config/getData";
 
@@ -23,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+      <Analytics />
+    </>
   );
 }
